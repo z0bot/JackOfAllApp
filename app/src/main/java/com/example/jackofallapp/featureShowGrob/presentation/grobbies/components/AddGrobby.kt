f505b19e4d8249c8754d00bf66bf3a5ae7ee8b58
z0bot <z_series@protonmail.ch>
2023-02-18 15:06:33 -0600

    I sure love you little honcho wonky<3

--- app/src/main/java/com/example/jackofallapp/featureShowGrob/presentation/grobbies/components/AddGrobby.kt
+++ app/src/main/java/com/example/jackofallapp/featureShowGrob/presentation/grobbies/components/AddGrobby.kt
@@ -1,13 +1,31 @@
 package com.example.jackofallapp.featureShowGrob.presentation.grobbies.components
 
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 
 @Composable
 fun AddGrobby(openDialog: Boolean){
     AlertDialog(
-        onDismissRequest = openDialog;
-    ){
-
-    }
+        onDismissRequest = {},
+        confirmButton = {
+            TextButton(onClick = {}) {
+                Text(text = "Add Grobby")
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = { /*TODO*/ }) {
+                Text(text = "Cancel")
+            }
+        },
+        title = {
+            Text(text = "New Grobby")
+        },
+        text = {
+            Text(text = "Name")
+            TextField(value = , onValueChange = )
+            Text(text = "Age")
+        })
 }
